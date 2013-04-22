c6b5132629944af2dec43d51bc1e11c4a9023241
lutfianasari <lutefnyew@gmail.com>
2013-04-22 15:50:35 +0900

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,8 +28,9 @@ describe User do
 
   it { should be_valid }
 
-  
   it { should respond_to(:authenticate) }
+
+  it { should respond_to(:remember_token) }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -105,5 +106,10 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
